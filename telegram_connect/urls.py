a7ad96386c34139070e311e4266e6a69fbcf5b3c
sasori <cacopu06071998@gmail.com>
2023-04-02 19:45:50 +0300

    change_branch

--- telegram_connect/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('check-login/', check_login, name='check_login'),
-    path('check-email/', check_email, name='check_email'),
-    path('register-user/', register_user, name='register_user'),
-]
